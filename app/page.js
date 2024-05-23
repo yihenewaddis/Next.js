08a8374e688b58b780767fddf08a59f26ca83d53
yihenew <yihenewu89@gmail.com>
2024-05-23 21:08:37 +0300

    add nav bar

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <>
-      <h1>this is yihenew</h1>
+      <h1>Home page</h1>
     </>
   );
 }
---
+++ components/Navbar.jsx
@@ -0,0 +1,19 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='flex  justify-between bg-slate-50 text-purple-500 py-5'>
+        
+        <div className="w-8" >Yihenew</div>
+
+        <nav className='blur-none font-medium  mx-10'>
+            <Link className='hover:text-red-600 transition-colors mr-10 blur-none' href='/'>Home</Link>
+            <Link className='hover:text-red-600 transition-colors blur-none' href='/signup'>signup</Link>
+        </nav>
+
+    </div>
+  )
+}
+
+export default Navbar
