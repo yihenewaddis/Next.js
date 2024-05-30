36c728395358fe35d8d65a96ce49bafca9cb1595
yihenew <yihenewu89@gmail.com>
2024-05-30 23:45:08 +0300

    some contents are removed

--- app/[id]/page.jsx
+++ app/[id]/page.jsx
@@ -10,8 +10,7 @@ export default function DetailPage({params}) {
       <p>and this is another name</p>
       <h1>this is yihenew addis</h1>
       <p>and this is another name</p>
-      <h1>this is yihenew addis</h1>
-      <p>and this is another name</p>
+
     </div>
   )
 }
